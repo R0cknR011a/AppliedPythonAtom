6f23cec9a703af7303b8a81d9bd6015e90c49bb1
R0cknR011a <myname@email.com>
2019-03-02 02:45:14 +0300

    Inverted integer.01

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -9,6 +9,15 @@ def reverse(number):
     :param number: исходное число
     :return: инвертированное число
     '''
-    while number % 10 == 0:
-        number = number / 10
-    return (str(int(number)))[::-1]
+    positive = False
+    string_number = ''
+    if number != 0:
+        if number > 0:
+            positive = True
+        number = abs(number)
+        while number % 10 == 0:
+            number = number / 10
+        string_number = (str(int(number)))[::-1]
+        return string_number if positive else '-' + string_number
+    else:
+        return 0
