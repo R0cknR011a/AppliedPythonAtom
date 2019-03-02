33499b15b1f634ec5cd67d26b5c31efc9da49281
myemail@sobaka.com <myemail@sobaka.com>
2019-03-02 14:34:26 +0300

    Palindrom added...

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -10,4 +10,8 @@ def check_palindrom(input_string):
     :return: True, если строка являестя палиндромом
     False иначе
     '''
-    raise NotImplementedError
+    inversed_string = str(input_string[::-1])
+    if inversed_string == input_string:
+        return True
+    else:
+        return False
