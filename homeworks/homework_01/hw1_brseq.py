7fe706202b243fd4d461c996e5db1a46f6819d29
R0cknR011a <myname@email.com>
2019-03-03 03:02:45 +0300

    Bracket Sequence 0.2 is added...

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -10,7 +10,23 @@ def is_bracket_correct(input_string):
     :param input_string: строка, содержащая 6 типов скобок (,),[,],{,}
     :return: True or False
     '''
-    if input_string == input_string[::-1]:
-        return True
-    else:
-        return False
+    for i in range(len(input_string)):
+        if input_string[i] == '(':
+            if input_string[i+1] == ']' or '}':
+                return False
+        if input_string[i] == '[':
+            if input_string[i+1] == ')' or '}':
+                return False
+        if input_string[i] == '{':
+            if input_string[i+1] == ')' or ']':
+                return False
+        if input_string.count('(') == input_string.count(')'):
+            if input_string.count('[') == input_string.count(']'):
+                if input_string.count('{') == input_string.count('}'):
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return False
