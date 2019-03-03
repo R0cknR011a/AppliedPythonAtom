10d0d5b9f618fdaab72f0c77748abc30c53179a5
myemail@sobaka.com <myemail@sobaka.com>
2019-03-03 21:41:15 +0300

    Det 0.1 is added...

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -11,4 +11,25 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    raise NotImplementedError
+    for i in range(len(list_of_lists)):
+        if len(list_of_lists) != len(list_of_lists[i]):
+            return None
+    det = 0
+    for l in range(len(list_of_lists)):
+        if l % 2 == 0:
+            sign = 1
+        else:
+            sign = -1
+        m = []
+        for i in range(1, len(list_of_lists)):
+            m.append([])
+            for j in range(0, len(list_of_lists[i])):
+                if j == l:
+                    continue
+                else:
+                    m[i - 1].append(list_of_lists[i][j])
+        if len(m) == 1:
+            det += sign * list_of_lists[0][l] * m[0][0]
+        else:
+            det += sign * list_of_lists[0][l] * calculate_determinant(m)
+    return det
