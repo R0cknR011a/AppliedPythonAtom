b26c6b356c2eb6dd8c1ae9b70870a9f572194c22
myemail@sobaka.com <myemail@sobaka.com>
2019-03-10 21:41:34 +0300

    Table is added...

--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -1,8 +1,24 @@
 import sys
-
+import json
+import csv
 # Ваши импорты
-
+from file_extension import extension
+from data_print import printer
 if __name__ == '__main__':
     filename = sys.argv[1]
 
     # Ваш код
+    encode = filename.split('-')[-1].split('.')[0]
+    if extension(filename, encode) == 'json':
+        with open(filename, encoding=encode) as f:
+            data = json.load(f)
+            m = [list(data[0].keys())]
+            for i in data:
+                m.append(list(i.values()))
+            printer(m)
+    elif extension(filename, encode) == 'tsv':
+        with open(filename, encoding=encode) as f:
+            data = csv.reader(f, delimiter='\t')
+            printer(list(data))
+    elif extension(filename, encode) is None:
+        print('File extension not valid')
