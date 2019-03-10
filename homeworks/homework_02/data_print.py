d1d2b22a70be8065f3998fd20ebdca14c0d56c0d
myemail@sobaka.com <myemail@sobaka.com>
2019-03-11 01:10:05 +0300

    Table 0.11 is added...

--- homeworks/homework_02/data_print.py
+++ homeworks/homework_02/data_print.py
@@ -3,21 +3,21 @@ def printer(data):
     for i in range(len(data[0])):
         max = 0
         for j in data:
-            if type(j[i]) is str and len(j[i]) >= max:
-                max = len(j[i])
+            if type(j[i]) is str and len(str(j[i])) >= max:
+                max = len(str(j[i]))
         m.append(max)
     print('-' * (sum(m) + (len(data[0]) - 1) * 5 + 6))
     for i in data:
         k = 0
         for j in i:
             if data.index(i) == 0:
-                print('|  ' + ' ' * ((m[k] - len(j))//2) + j +
-                      ' ' * ((m[k] - len(j))//2), end='  ')
+                print('|  ' + ' ' * ((m[k] - len(str(j)))//2) + str(j) +
+                      ' ' * ((m[k] - len(str(j)))//2), end='  ')
             else:
-                if j.isdigit():
-                    print('|  ' + ' ' * (m[k] - len(j)) + j, end='  ')
-                else:
-                    print('|  ' + j + ' ' * (m[k] - len(j)), end='  ')
+                if str(j).isdigit():
+                    print('|  ' + ' ' * (m[k] - len(str(j))) + str(j), end='  ')
+                if type(j) is str:
+                    print('|  ' + str(j) + ' ' * (m[k] - len(str(j))), end='  ')
             k += 1
         print('|')
     print('-' * (sum(m) + (len(data[0]) - 1) * 5 + 6))
