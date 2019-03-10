b08626fb07ec6c2c06bbc40c88b2c9f8cb943769
myemail@sobaka.com <myemail@sobaka.com>
2019-03-10 21:47:48 +0300

    Table is added...

---
+++ homeworks/homework_02/data_print.py
@@ -0,0 +1,20 @@
+def printer(data):
+    m = []
+    for i in range(len(data[0])):
+        max = 0
+        for j in data:
+            if type(j[i]) is str and len(str(j[i])) >= max:
+                max = len(str(j[i]))
+        m.append(max)
+    print('-' * (sum(m) + (len(data[0]) - 1) * 3 + 4))
+    for i in data:
+        k = 0
+        for j in i:
+            if data.index(i) == 0:
+                print('| ' + ' ' * ((m[k] - len(str(j)))//2) + str(j) +
+                      ' ' * ((m[k] - len(str(j)))//2), end = ' ')
+            else:
+                print('| ' + str(j) + ' ' * (m[k] - len(str(j))), end = ' ')
+            k += 1
+        print('|')
+    print('-' * (sum(m) + (len(data[0]) - 1) * 3 + 4))
