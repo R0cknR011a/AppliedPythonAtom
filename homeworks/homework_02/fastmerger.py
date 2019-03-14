fc4d3ab2c601b8bb6f137ba4eb8c0f7658891dbb
R0cknR011a <myname@email.com>
2019-03-14 03:36:12 +0300

    FastMerger is done

--- homeworks/homework_02/fastmerger.py
+++ homeworks/homework_02/fastmerger.py
@@ -12,4 +12,13 @@ class FastSortedListMerger:
         принимает на вход список отсортированных непоубыванию списков и число
         на выходе выдает один список длинной k, отсортированных по убыванию
         '''
-        raise NotImplementedError
+        m = MaxHeap([])
+        result = []
+        print(list_of_lists, k)
+        for i in list_of_lists:
+            for j in i:
+                m.add((j, list_of_lists.index(i)))
+        print(m.heap)
+        for i in range(k):
+            result.append(m.extract_maximum()[0])
+        return result
