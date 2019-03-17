fc40cf19d0aa36cfe52efd75da2b8105a1f53330
myemail@sobaka.com <myemail@sobaka.com>
2019-03-18 02:58:31 +0300

    LRUCache is added...

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
+import time
+from collections import OrderedDict
 
 
 class LRUCacheDecorator:
@@ -12,8 +14,39 @@ class LRUCacheDecorator:
         '''
         # TODO инициализация декоратора
         #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+        self.maxsize = maxsize
+        self.dict = OrderedDict()
+        self.start = time.time()
+        if ttl is None:
+            self.ttl = 9999999
+        else:
+            self.ttl = ttl
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, f):
         # TODO вызов функции
-        raise NotImplementedError
+        def func(*args, **kwargs):
+            m = []
+            for i in args:
+                m.append(i)
+            for i, j in kwargs.items():
+                m.append(i, j)
+            input = ''.join(str(x) for x in m)
+            if time.time() - self.start < self.ttl:
+                if input in self.dict:
+                        if len(self.dict) == self.maxsize:
+                            x = self.dict[input]
+                            del self.dict[input]
+                            self.dict[input] = x
+                        return self.dict[input]
+                else:
+                    if len(self.dict) == self.maxsize:
+                        delete = list(self.dict.keys())[0]
+                        del self.dict[delete]
+                    self.dict[input] = f(*args, **kwargs)
+                    return f(*args, **kwargs)
+            else:
+                self.dict.clear()
+                self.start = time.time()
+                self.dict[input] = f(*args, **kwargs)
+                return f(*args, **kwargs)
+        return func
